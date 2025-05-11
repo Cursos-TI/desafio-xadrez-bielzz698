2cc41c0ad7ce52b43781ee0978e239bc313c5f27
bielzz698 <biel.ferreira3938@gmail.com>
2025-05-11 15:53:50 +0000

    Eu Criei o Printf

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,8 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+    printl ("desafio xadez!\n")
+}
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
